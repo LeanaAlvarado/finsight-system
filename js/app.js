42a42e0640e73915902d12169afce7efaa48761d
LeanaAlvarado <anael081787@gmail.com>
2026-07-27 17:17:34 +0800

    Remove feedback rating helper note

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260727-feedback-rating-select-v28";
+const appVersion = "20260727-feedback-rating-no-note-v29";
 
 function toTitleCase(value = "") {
   return String(value || "User")
