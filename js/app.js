68c2dd01cfb3d651965889bdd3cc9cd4b0f903f4
LeanaAlvarado <anael081787@gmail.com>
2026-07-29 16:29:58 +0800

    Move edit duration into manpower details

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260729-edit-duration-visible-v55";
+const appVersion = "20260729-edit-duration-panel-v56";
 const approvedRoleLabels = new Set([
   "System Administrator",
   "Owner/Manager",
