4bd34cdc8fd3a4c14203af7462bb9e680426b842
LeanaAlvarado <anael081787@gmail.com>
2026-08-03 14:09:59 +0800

    Fix dashboard project materials panel CSS

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260803-cctv-material-dashboard-v89";
+const appVersion = "20260803-dashboard-material-css-v90";
 const approvedRoleLabels = new Set([
   "System Administrator",
   "Owner/Manager",
