039d1a3781f0f437769907b6583b0f6cc481c82a
LeanaAlvarado <anael081787@gmail.com>
2026-08-07 19:08:04 +0800

    Align five revenue summary cards

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260807-revenue-four-kpi-v101";
+const appVersion = "20260807-revenue-five-kpi-v102";
 const approvedRoleLabels = new Set([
   "System Administrator",
   "Owner/Manager",
