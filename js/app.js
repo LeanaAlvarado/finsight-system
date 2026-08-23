e9b8ca7a044727c413215cdf26af8eb792d27148
LeanaAlvarado <anael081787@gmail.com>
2026-08-23 19:31:56 +0800

    Prevent revenue amount overlap

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260823-operations-active-projects-v249";
+const appVersion = "20260823-revenue-no-overlap-v250";
 const approvedRoleLabels = new Set([
   "System Administrator",
   "Owner/Manager",
