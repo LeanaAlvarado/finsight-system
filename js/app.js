0540abf67582884f7afd5fb82db1c81fc5dacf59
LeanaAlvarado <anael081787@gmail.com>
2026-08-23 18:48:50 +0800

    Make financial table labels fully visible

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const pageModules = {
 const publicPages = new Set(["index.html", "public-feedback.html"]);
 const pageName = window.location.pathname.split("/").pop() || "index.html";
 const pageModule = pageModules[pageName];
-const appVersion = "20260823-project-name-v239";
+const appVersion = "20260823-table-labels-v240";
 const approvedRoleLabels = new Set([
   "System Administrator",
   "Owner/Manager",
